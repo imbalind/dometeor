c44be93bbc4cee68703685acd81c2b5911628ac1
imbalind <matteobrassini@gmail.com>
2015-05-19 21:59:29 +0200

    Added NON SECURE implementation to edit from python client.

--- meteoranti.js
+++ meteoranti.js
@@ -5,20 +5,26 @@ if (Meteor.isClient) {
   Template.body.events({
       "submit .new-event": function (event) {
           
-       var text = "Sensor X fired at " + new Date();   
+        Meteor.call("addEvent",0,true);
           
-       Events.insert({
-           event_text: text,
-           createdAt: new Date()
-       });
+     
        return false;
       }
   });
     
   Template.body.helpers({
     events: function() {
-        return Events.find({});   
+        return Events.find({owner: Meteor.userId()},{sort: {createdAt: -1}});   
     }
+  });
+    
+  Template.event.events({
+     "click .toggle-checked": function () {
+        Events.update(this._id, {$set: {checked: ! this.checked}});   
+     },
+     "click .delete": function () {
+        Events.remove(this._id); 
+     }
   });
 }
 
@@ -27,3 +33,21 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Meteor.methods({
+    addEvent: function (sensor_num, value) {
+        
+        if(! Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");   
+        }
+        
+        var text = "Sensor #"+ sensor_num +" fired with value: " + value;
+        
+        Events.insert({
+            event_text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    }
+})
