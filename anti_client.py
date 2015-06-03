46f367dca1d92a30bd88f328f42ecf5cfb2d1d05
imbalind <matteobrassini@gmail.com>
2015-06-03 13:27:29 +0200

    Added onConnect callback and logging library.

--- anti_client.py
+++ anti_client.py
@@ -1,29 +1,62 @@
 import RPi.GPIO as GPIO
 import time
 import yaml
+import logging
 from MeteorClient import MeteorClient
+
+
 
 # Sensor interrupt callback
 def my_callback(channel):
     global client
-    print('Channel: '+ str(channel) + ' Time: ' + time.ctime())
+    logging.warning('Channel: '+ str(channel) + ' Time: ' + time.ctime())
     client.call('addEvent',[channel,True])
 
+    
+    
 # Function to filter only interrupting sensors
 def getInterruptingOnly(sensor):
-   return sensor.get('type') == 'antitheft'
+    return sensor.get('type') == 'antitheft'
+
+
+
+# Register connection to start alarm on connection closed
+def onConnected():
+    logging.info ('Login...')
+    client.login(username,password)
+    logging.info ('Registering connection - starting')
+    client.call('registerAlarmConnection',[])
+    logging.info ('Registering connection - done')
+  
+    
+    
+# Register sensor callback
+def registerSensorCallback(sensor):
+    channel = sensor.get('channel')
+    name = sensor.get('sensor')
+    logging.info ('Setting callback for channel: ' + str(channel) + ' - ' + str(name))
+    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=300)
+
+
+
+# Setting up logging
+logging.basicConfig(filename='info.log',level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
+
+
 
 # Loading configurations
-print 'Loading configurations'
+logging.info ('Loading configurations')
 try:
     configFile = open('.config')
+    
 except IOError:
-    print 'No .config file!'
+    logging.error ('No .config file!')
     exit()
 
 config = yaml.safe_load(configFile)
 if config is None:
-    print '.config file is empty'
+    logging.error ('.config file is empty')
     exit()
 
 host = config.get('host')
@@ -31,60 +64,55 @@ username = config.get('username')
 password = config.get('password')
 
 if host is None or username is None or password is None:
-    print 'Host, username or password are missing in .config file'
+    logging.error ('Host, username or password are missing in .config file')
     exit()
 
+    
 
 # Loading sensors
-print 'Loading sensors'
+logging.info ('Loading sensors')
 try:
     sensorsFile = open('.sensors')
+    
 except IOError:
-    print 'No .sensors file!'
+    logging.error ('No .sensors file!')
     exit()
-
+    
 sensors = yaml.safe_load(sensorsFile)
 if sensors is None:
-    print '.sensors file is empty'
+    logging.error ('.sensors file is empty')
     exit()
-
+    
 sensorList = sensors.get('sensors')
 username = config.get('username')
 password = config.get('password')
 
 if not sensorList:
-    print 'No sensor defined in .sensors'
+    logging.error ('No sensor defined in .sensors')
     exit()
 
-
-
+    
+    
 # Begin main application
+logging.info ('Setting meteor service')
 global client
 client = MeteorClient(host)
-print 'Connecting to service'
+logging.info ('Connecting to service')
 client.connect()
-print 'Login...'
-client.login(username,password)
-print 'Registering connection - starting'
-client.call('registerAlarmConnection',[]);
-print 'Registering connection - done'
+client.on('connected', onConnected)
+client.on('reconnected', onConnected)
 
 try:
-
-    print 'Setting GPIO and start listening to sensors'
+    logging.info ('Setting GPIO and start listening to sensors')
     GPIO.setmode(GPIO.BCM)
-
     for sensor in filter(getInterruptingOnly,sensorList):
-      channel = sensor.get('channel')
-      name = sensor.get('sensor')
-      print 'Setting callback for channel: ' + str(channel) + ' - ' + str(name) 
-      GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-      GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=300)
+        registerSensorCallback(sensor)
 
     while True:
         time.sleep(100)
+        
 except KeyboardInterrupt:
     GPIO.cleanup()
     client.logout()
     client.close()
-    print 'Exiting after cleanup!'
+    logging.warning ('Exiting after cleanup!')
