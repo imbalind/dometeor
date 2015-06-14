db605bf8ea9a96778ca6411331d3f1d5a6cd639a
imbalind <matteobrassini@gmail.com>
2015-06-15 00:17:30 +0200

    Added alarm users and owner-alarm links.

--- meteoranti.js
+++ meteoranti.js
@@ -1,26 +1,67 @@
 Events = new Mongo.Collection("events");
 Status = new Mongo.Collection("status");
+Links = new Mongo.Collection("links");
 
 if (Meteor.isClient) {
   // This code only runs on the client
     
     Meteor.subscribe("events");
 	Meteor.subscribe("status");
+    Meteor.subscribe("links");
+    Meteor.subscribe("emails");
 
   Template.body.helpers({
-    events: function() {
-        return Events.find({},{sort:{createdAt: -1},limit:10});   
-    },
+    
 	alarmOn: function() {
-		alarmStatus = Status.findOne({});
+		alarmStatus = Status.findOne({owner : Meteor.userId()});
 		if (!alarmStatus) {
 			return;
 		}
 		return alarmStatus.isOn;
-	}
+	}, 
+    isAlarm: function() {
+        return Meteor.user() && Meteor.user().isAlarm;
+    }
   });
     
+  Template.tabs.helpers({
+    events: function() {
+        return Events.find({},{sort:{createdAt: -1},limit:10});   
+        //return null;
+    },
+    linkedAlarms: function() {
+        return Links.findOne({owner: Meteor.userId()}).alarms;
+    },
+    linkedClients: function() {
+        return Links.find({"alarms.alarmId": Meteor.userId()});
+    },
+    isClient: function() {
+        return Meteor.user() && !Meteor.user().isAlarm;
+    },
+    isAlarm: function() {
+        return Meteor.user() && Meteor.user().isAlarm;
+    }
+  });
+    
+  Template.event.helpers({
+    email: function() {
+        return Meteor.users.findOne({_id: this.owner}).emails[0].address;
+    } 
+  });
 
+  Template.linkedAlarm.helpers({
+    alarmOn: function() {
+		alarmStatus = Status.findOne({owner: this.alarmId});
+		if (!alarmStatus) {
+			return;
+		}
+		return alarmStatus.isOn;
+	},  
+    email: function() {
+        return Meteor.users.findOne({_id: this.alarmId}).emails[0].address;
+    } 
+  });
+    
     
   Template.body.events({
       "click #new_event": function (event) {
@@ -32,22 +73,49 @@ if (Meteor.isClient) {
     
   Template.body.events({
 	"change #switch_on_off": function (e) {
-		Meteor.call("turnOnOff", e.target.checked);
-	}
+		Meteor.call("turnOnOff", Meteor.userId(), e.target.checked);
+	},
+    "submit #add-client": function (e) {
+        emailAddress = e.target.email.value;
+        e.target.email.value="";
+        Meteor.call("linkAlarmTo", emailAddress);
+        event.preventDefault();
+    }
   });
 	
   Template.event.events({
      "click .toggle-checked": function () {
-         
+        
         Meteor.call("toggleChecked",this._id, this.checked);
           
      },
      "click #delete": function () {
-         
         Meteor.call("deleteEvent",this._id); 
      }
   });
-}
+    
+  Template.linkedAlarm.events({
+     "click #delete": function () {
+        Meteor.call("deleteLink",Meteor.userId(), this.alarmId); 
+     },
+     "change #alarm_on_off": function (e) {
+		Meteor.call("turnOnOff", this.alarmId, e.target.checked);
+	}
+  });
+    
+    
+  Template.linkedClient.events({
+     "click #delete": function () {
+        Meteor.call("deleteLink", this.owner, Meteor.userId()); 
+     }
+  });
+    
+  Template.tabs.onRendered(function(){ 
+
+  $('ul.tabs').tabs() 
+
+  });
+}    
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -55,13 +123,40 @@ if (Meteor.isServer) {
   });
 
     Meteor.publish("events", function() {
-		return Events.find({owner: this.userId},{sort:{createdAt: -1},limit:10});   
+        link = Links.findOne({owner: this.userId});
+        if(!link) {
+            alarms = [this.userId];
+        } else {
+            alarms = link.alarms.map(function(alarm) {
+                return alarm.alarmId;
+            });
+        }
+        return Events.find({owner: {$in : alarms}},{sort:{createdAt: -1},limit:10});   
     });
 
 	Meteor.publish("status", function() {
-		return Status.find({owner: this.userId});   
+        link = Links.findOne({owner: this.userId});
+        if(!link) {
+            alarms = [this.userId];
+        } else {
+            alarms = link.alarms.map(function(alarm) {
+                return alarm.alarmId;
+            });
+        }
+        return Status.find({owner: {$in : alarms}});   
     });
-	
+    
+    Meteor.publish("links", function() {
+		return Links.find();
+    });    	
+    
+    Meteor.publish("emails", function() {
+        return Meteor.users.find({},{fields : {emails:1}});
+    });
+    
+    Meteor.publish(null, function() {
+        return Meteor.users.find({_id: this.userId},{fields : {"isAlarm":1}});
+    });
 }
 
 
@@ -95,14 +190,36 @@ Meteor.methods({
     deleteEvent: function(event_id) {
         Events.remove(event_id);    
     },
+    
+    deleteLink: function(client_id, alarm_id) {
+        link = Links.findOne({owner : client_id});
+        if (link.alarms.length <= 1) {
+            Links.remove({owner : client_id});
+        } else {
+            Links.update({owner : client_id},{$pull:{"alarms.alarm_id" : alarm_id}});
+        }
+    },
 	
-	turnOnOff: function (turnOn) {
-		Status.update({owner : Meteor.userId()},{$set: {isOn : turnOn}}, {upsert : true});
+	turnOnOff: function (userId, turnOn) {
+		Status.update({owner : userId},{$set: {isOn : turnOn}}, {upsert : true});
 	},
     
     registerAlarmConnection: function() {
         this.connection.onClose(function() {
             Meteor.call("addEvent",-1, "Disconnected");
         });
+    },
+    
+    linkAlarmTo: function (ownerEmail) {
+        
+        alarmId = Meteor.userId();
+        ownerId = Meteor.users.findOne({"emails.address":ownerEmail},{fields : {"_id":1}})._id;
+        
+        if(ownerId && !Links.findOne({owner: ownerId, "alarms.alarmId": alarmId})) {
+            newAlarm = {};
+            newAlarm.alarmId = alarmId;
+            newAlarm.confirmed = false;
+            Links.update({owner : ownerId}, {$addToSet: {alarms: newAlarm},$set: {email: ownerEmail}}, {upsert : true});
+        }
     }
 })
